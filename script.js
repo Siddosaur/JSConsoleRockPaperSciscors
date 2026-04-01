8df84e531c23b7a00de7e1b2b85132cedee26eea
Siddosaur <dinosaursrsupercool@gmail.com>
2026-04-01 17:36:18 -0400

    STEP 2

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hey guys")
+function getComputerChoice() {
+    rnum = Math.random()
+    if (rnum < 0.33333) {
+        return "Paper"
+    }
+    else if (rnum > 0.66666) {
+        return "Rock"
+    }
+    else {
+        return "Scissor"
+    }
+}
+console.log(getComputerChoice())
